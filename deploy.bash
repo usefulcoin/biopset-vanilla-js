97a6180884a07797fa0d0f8676027d5d224eb9b6
munair <munair@gmail.com>
2021-05-09 13:45:37 +0900

    add deploy script

---
+++ deploy.bash
@@ -0,0 +1,18 @@
+#! /bin/bash
+#
+# script name: deploy.bash
+# script author: munair simpson
+# script created: 20210509
+# script purpose: deploy biopset-vanilla-js to gh-pages or S3 bucket
+
+read -p "Push to staging on gh-pages? (https enabled by default) [yes]: " staging
+read -p "Push to production on AWS S3? (https not enabled by default) [yes]: " production
+
+staging=${staging:-"yes"}
+production=${production:-"yes"}
+
+# step 1: push to GitHub Pages (**staging**)
+if [ $staging == "yes" ]; then git push origin main:gh-pages ; fi
+
+# step 2: copy to S3 bucket (**production**)
+if [ $production == "yes" ]; then aws s3 sync . s3://biopset-vanilla-js ; fi
